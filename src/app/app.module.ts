64f657dc0dd9343c65f67fa8a39b9a8cbccb4433
Muhammad Al-Aasar <70108754+Muhammad-Alaasar@users.noreply.github.com>
2024-06-12 02:05:33 +0300

    First upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ConversationsComponent } from './components/conversations/conversations.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ConversationsComponent,
+    ChatComponent,
+    UserProfileComponent,
+    CategoriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
